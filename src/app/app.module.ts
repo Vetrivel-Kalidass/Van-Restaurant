3b84907df114684c167814794c11239318d5b3c2
Vetrivel Kalidass <vetrivelkalidass10700@gmail.com>
2021-10-02 14:30:30 +0530

    Added Fresh Project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,76 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MenuComponent } from './menu/menu.component';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { AboutComponent } from './about/about.component';
-import { DishService } from './services/dish.service';
-import { LeaderService } from './services/leader.service';
-import { PromotionService } from './services/promotion.service';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
-import { LoginComponent } from './login/login.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSelectModule } from '@angular/material/select';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatSliderModule} from '@angular/material/slider';
-import { baseURL } from './shared/baseurl';
-import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    MenuComponent,
-    DishdetailComponent,
-    AboutComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    ContactComponent,
-    LoginComponent,
-    HighlightDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    MatToolbarModule,
-    FlexLayoutModule,
-    MatListModule,
-    MatGridListModule,
-    MatCardModule,
-    MatButtonModule,
-    AppRoutingModule,
-    MatDialogModule,
-    FormsModule,
-    MatInputModule,
-    MatCheckboxModule,
-    MatFormFieldModule,
-    ReactiveFormsModule,
-    MatSlideToggleModule,
-    MatSelectModule,
-    MatProgressSpinnerModule,
-    MatSliderModule,
-    HttpClientModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
-  entryComponents: [LoginComponent],
-  providers: [DishService, LeaderService, PromotionService, { provide: 'BaseURL', useValue: baseURL }],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
